931e49107308fda5eb245633ed87536f91104459
Fagner Santiago <fagner.santiago@hotmail.com>
2022-01-13 16:24:00 -0300

    teste finalizado

--- src/modules/statements/useCases/getStatementOperation/GetStatementOprationUseCase.spec.ts
+++ src/modules/statements/useCases/getStatementOperation/GetStatementOprationUseCase.spec.ts
@@ -1,7 +1,57 @@
-//TODO
-// describe(()=>{
-//   beforeEach(()=>{
+import { InMemoryUsersRepository } from "../../../users/repositories/in-memory/InMemoryUsersRepository";
+import { IUsersRepository } from "../../../users/repositories/IUsersRepository";
+import { InMemoryStatementsRepository } from "../../repositories/in-memory/InMemoryStatementsRepository";
+import { IStatementsRepository } from "../../repositories/IStatementsRepository";
+import { GetStatementOperationError } from "./GetStatementOperationError";
+import { GetStatementOperationUseCase } from "./GetStatementOperationUseCase";
 
-//   })
-//   it("")
-// })
+let getStatementOperationUseCase: GetStatementOperationUseCase;
+let usersRepository: IUsersRepository;
+let statements: IStatementsRepository;
+
+enum OperationType {
+  DEPOSIT = "deposit",
+  WITHDRAW = "withdraw",
+}
+describe("Get Statement", () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository();
+    statements = new InMemoryStatementsRepository();
+
+    getStatementOperationUseCase = new GetStatementOperationUseCase(
+      usersRepository,
+      statements
+    );
+  });
+  it("Should be able to list statment by id", async () => {
+    const user = await usersRepository.create({
+      name: "jhon doe",
+      email: "email@com",
+      password: "123456",
+    });
+
+    const statementResponse = await statements.create({
+      user_id: user.id as string,
+      type: OperationType.WITHDRAW,
+      amount: 100,
+      description: "depósito",
+    });
+
+    const statement = statementResponse.id as string;
+
+    const response = await getStatementOperationUseCase.execute({
+      user_id: user.id as string,
+      statement_id: statement,
+    });
+    expect(response).toHaveProperty("id");
+  });
+
+  it("Should not be get statement non-existents user", async () => {
+    expect(async () => {
+      await getStatementOperationUseCase.execute({
+        user_id: "1524645",
+        statement_id: "112313",
+      });
+    }).rejects.toBeInstanceOf(GetStatementOperationError.UserNotFound);
+  });
+});
